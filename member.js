4811578f29cb7c5c63935f0caf09cbdda72bd671
Justin Cooper <49234323+JustinotherGitter@users.noreply.github.com>
2024-05-06 12:41:49 +0000

    completion panel commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+  var skills = [
+    {
+      name: 'HTML',
+      percentage: 90
+    },
+    {
+      name: 'CSS',
+      percentage: 80
+    },
+    {
+      name: 'JavaScript',
+      percentage: 70
+    },
+    {
+      name: 'React',
+      percentage: 60
+    },
+    {
+      name: 'NodeJS',
+      percentage: 50
+    }
+  ];
+  return skills;
+}
